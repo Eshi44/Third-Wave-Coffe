a6b84cf126edc265da65e0bff73bfe8de290c779
Eshi44 <e.hameliervolino@gmail.com>
2020-05-07 10:34:16 -0400

    added shared footer and main container file

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from "axios";
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./components/Shared/Footer";
+// import logo from './logo.svg';
+// import './App.css';
+// import axios from "axios";
 
 function App() {
-  useEffect(() => {
-    axios
-      .get("/api/config")
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Footer/>
+      </Router>
+      </>
   );
 }
 
---
+++ client/src/components/Shared/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Footer.css"
+
+class Footer extends Component {
+	render() {
+		return (
+			<>
+				<footer>
+					<p>Copyright © Third Wave Coffee</p>
+				</footer>
+			</>
+		);
+	}
+}
+
+export default Footer;
