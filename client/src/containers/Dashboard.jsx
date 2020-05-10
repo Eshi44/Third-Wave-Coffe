876181c86d07eca03453e373ae7518807e8739fb
Eshi44 <e.hameliervolino@gmail.com>
2020-05-10 10:51:38 -0400

    added chemex, aeropress and frenchpress images to dashboard

--- client/src/containers/Dashboard.jsx
+++ client/src/containers/Dashboard.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import NavBar from "../components/Shared/NavBar"
+import Chemex from "../images/chemex.png"
+import Aeropress from "../images/aeropress.png"
+import Frenchpress from "../images/frenchpress.png"
 
 
 class Dashboard extends Component {
@@ -6,7 +10,16 @@ class Dashboard extends Component {
         return (	
 
         <>
-        <h3>User</h3>
+        <NavBar />
+
+        <div className="container">
+					<div className="row " id="coffee-rows">
+						<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center" ><img className="img-responsive" src={Aeropress} alt="Aeropress" /></div>
+						<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center" ><img className="img-responsive" src={Chemex} alt="Chemex"/></div>
+						<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center" ><img className="img-responsive" src={Frenchpress} alt="Frenchpress" /></div>
+					</div>
+				</div>
+        
         </>
     );
 	}
