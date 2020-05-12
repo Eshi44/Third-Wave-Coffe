9de5bb4b7e71b625f583fcf37b1d30b3b45d4786
Eshi44 <e.hameliervolino@gmail.com>
2020-05-12 11:08:19 -0400

    added first input to preferences page

--- client/src/containers/Preferences/Preferences.jsx
+++ client/src/containers/Preferences/Preferences.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Header from "../../components/Shared/Header/Header";
 import "./Preferences.css";
+import SmallCup from "../../images/small-cup.png";
+import MedCup from "../../images/medium-cup.png";
+import LargeCup from "../../images/large-cup.png";
 
 class Chemex extends Component {
 	constructor() {
@@ -21,23 +24,63 @@ class Chemex extends Component {
 				<div className="container" id="cardCalculation">
 					<div className="card" id="card-body">
 						<div className="card-body">
-							<h3 id="desiredText">Desired Quantity:</h3>
+							<h3 id="desiredTextFirst">Desired Quantity:</h3>
 							<div className="row">
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center" >Small</div>
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">Medium</div>
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">Large</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									<input
+										id="inputOne"
+										type="image"
+										className="img-responsive"
+										src={SmallCup}
+										alt="Small Cup"
+									/>
+									
+									<p id="textsml">Small</p>
+								</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									<input
+										id="inputTwo"
+										type="image"
+										className="img-responsive"
+										src={MedCup}
+										alt="Medium Cup"
+									/>
+									<p id="textsml">Medium</p>
+								</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									<input
+										id="inputThree"
+										type="image"
+										className="img-responsive"
+										src={LargeCup}
+										alt="Large Cup"
+									/>
+									<p id="textsml">Large</p>
+								</div>
 							</div>
 							<h3 id="desiredText">Desired Strength:</h3>
 							<div className="row">
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center" >Weak</div>
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">Medium</div>
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">Stong</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									Weak
+								</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									Medium
+								</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									Stong
+								</div>
 							</div>
 							<h3 id="desiredText">Desired Bean Roast:</h3>
 							<div className="row">
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center" >Light</div>
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">Medium</div>
-							<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">Dark</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									Light
+								</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									Medium
+								</div>
+								<div className="col col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+									Dark
+								</div>
 							</div>
 						</div>
 					</div>
