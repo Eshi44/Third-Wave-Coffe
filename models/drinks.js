f08b79592659f27f631d5c6e55279c8585c98086
Eshi44 <e.hameliervolino@gmail.com>
2020-05-11 21:38:29 -0400

    fixed models and added mongoose-unique-validator to check username is unique

--- models/drinks.js
+++ models/drinks.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const drinksSchema = new Schema({
-  user_id: { type: String, required: true },
-  method: { type: String },
-  size: { type: String },
-  strength: { type: String },
-  roast: { type: String },
-  rating: { type: Integer },
-  favorite: { type: Boolean},
-  notes: { type: String },
-  date: { type: Date, default: Date.now }
-  
+const DrinksSchema = new Schema({
+	user: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+	method: { type: String },
+	size: { type: String },
+	strength: { type: String },
+	roast: { type: String },
+	rating: { type: Integer },
+	favorite: { type: Boolean },
+	notes: { type: String },
+	date: { type: Date, default: Date.now },
 });
 
-const Drink = mongoose.model("Drink", drinksSchema);
+const Drink = mongoose.model("Drink", DrinksSchema);
 
 module.exports = Drink;
--- models/users.js
+++ models/users.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const mongooseUniqueValidator = require("mongoose-unique-validator");
 
-const usersSchema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+const UserSchema = new Schema({
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	date: { type: Date, default: Date.now },
+	drinks: [{ type: Schema.Types.ObjectId, ref: "Drink" }],
 });
 
-const User = mongoose.model("User", usersSchema);
+Schema.plugin(mongooseUniqueValidator);
+
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
