36dc6e55be9a7b9f2403259f18aafb09611d6fb3
Eshi44 <e.hameliervolino@gmail.com>
2020-05-12 13:40:45 -0400

    added history link to brew page

--- client/src/containers/Brew/Brew.jsx
+++ client/src/containers/Brew/Brew.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../../components/Shared/Header/Header";
-
+import History from "../../images/history.png";
+import { Link } from "react-router-dom";
 
 class Brew extends Component {
 	constructor() {
@@ -17,7 +18,21 @@ class Brew extends Component {
 		return (
 			<>
 				<Header name={this.state.name} />
-				
+
+				<div className="row">
+					<div className="col col-sm-4 col-md-4 col-lg-4"></div>
+					<div className="col col-sm-4 col-md-4 col-lg-4"></div>
+					<div className="col col-sm-4 col-md-4 col-lg-4" id="history">
+						<Link to="/history">
+							<img
+								className="img-responsive"
+								id="hover"
+								src={History}
+								alt="History"
+							/>
+						</Link>{" "}
+					</div>
+				</div>
 			</>
 		);
 	}
