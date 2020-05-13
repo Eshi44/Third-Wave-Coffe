bb143dd31de0024ac7a78a6c0ca3ebae1ff483aa
Eshi44 <e.hameliervolino@gmail.com>
2020-05-13 13:45:27 -0400

    added useUnifiedTopology:true to mongoose

--- server.js
+++ server.js
@@ -25,8 +25,7 @@ app.use("/api/user", AuthController);
 app.use("/api/posts", PostRoute);
 
 // mongoose connections
- mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coffeedb", { useNewUrlParser: true },  
- () => console.log("connected to db"));
+ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coffeedb", {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(express.static("client/build"));
 
